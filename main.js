4071cade9fcfe7dfcc87fb7d33a3b348fffeba4f
aryo faizi <aryofaizi@gmail.com>
2023-02-20 18:38:07 +0330

    remove logs and function in ready

--- main.js
+++ main.js
@@ -2,13 +2,11 @@ function findDuplicates(arr) {
     // Your code here
     let duplicates = [];
     for(item of arr){
-        console.log("item",item)
         let counter = 0;
         for(value of arr){
             if(value == item){
                 counter ++;
             }
-            console.log("counter",counter)
             if(counter >1 && !duplicates.includes(item)){
                 duplicates.push(item);
             }
